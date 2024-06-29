bb33320e13d0971b69604d05525f7029731d4288
roy nahmuka <royna@wix.com>
2024-06-29 15:38:59 +0300

    Implement the ECHO command

--- app/main.ts
+++ app/main.ts
@@ -6,8 +6,14 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
   // Handle connection
-  connection.on('data', data => {
-    connection.write('+PONG\r\n')
+  connection.on('data', (data: Buffer) => {
+    const args = data.toString().split('\r\n')
+    const command = args[2].toLowerCase()
+
+    switch(command) {
+        case 'ping': connection.write('+PONG\r\n')
+        case 'echo': connection.write(`$${args[4].length} ${args[4]}`)
+    }
   })
 });
 
